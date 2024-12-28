48652ccfdffee572ff25a94c8507ff2b3d98bd4e
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2024-12-28 19:09:40 +0530

    Update urls.py

--- todo_project/todo/urls.py
+++ todo_project/todo/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import register_view, login_view, logout_view, get_tasks, add_task, edit_task, delete_task
+from .views import register_view, login_view, logout_view, get_tasks, add_task, edit_task, delete_task,api_response
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('register/', register_view, name='register'),
@@ -9,5 +10,6 @@ urlpatterns = [
     path('tasks/add/', add_task, name='add_task'),
     path('tasks/edit/<int:task_id>/', edit_task, name='edit_task'),
     path('tasks/delete/<int:task_id>/', delete_task, name='delete_task'),
-
+    path('token/', obtain_auth_token, name='api_token_auth'),
+    path('status/', api_response, name='api_status'),  # New API status endpoint
 ]
