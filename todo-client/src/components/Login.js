2bba2929b0fb1497a5ae35676335d088166abf3d
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2024-12-28 19:07:52 +0530

    Update Login.js

--- todo-client/src/components/Login.js
+++ todo-client/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../App.css';
+import 'animate.css';
 
 const Login = () => {
   const [username, setUsername] = useState('');
@@ -19,6 +20,8 @@ const Login = () => {
 
       const data = await response.json();
       if (response.ok) {
+        localStorage.setItem('token', data.token);
+        setMessage(data.message);
         navigate('/tasks');
       } else {
         setMessage(data.error);
@@ -32,8 +35,8 @@ const Login = () => {
   return (
     <div className="auth-container">
       <form onSubmit={handleLogin} className="auth-form">
-        <h2>Login</h2>
-        <p class='text-danger bg-light'>{message}</p>
+      <h1 className="animate__animated animate__bounce">Login</h1>
+        <p className='text-danger bg-light'>{message}</p>
         <input
           type="text"
           placeholder="Username"
