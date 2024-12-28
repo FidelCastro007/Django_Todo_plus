ef5ab69b4e479a8db3779b3eb865229703f95aa0
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2024-12-28 19:09:11 +0530

    Update serializers.py

--- todo_project/todo/serializers.py
+++ todo_project/todo/serializers.py
@@ -1,52 +1,7 @@
-# from rest_framework import serializers
-# from .models import Task
-# from django.contrib.auth.models import User
-
-# class TaskSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Task
-#         fields = ['id', 'title', 'description', 'created_at', 'updated_at']
-
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['id', 'username', 'email']
-
-
-# from rest_framework import serializers
-# from .models import Task
-
-# class TaskSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Task
-#         fields = ['id', 'title', 'description']
-
-
 from rest_framework import serializers
-from django.contrib.auth import authenticate
-from .models import CustomUser
+from .models import Task  # Assuming you have a Task model
 
-class RegisterSerializer(serializers.ModelSerializer):
+class TaskSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
-        fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = CustomUser.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        return user
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
-
-    def validate(self, data):
-        user = authenticate(**data)
-        if user and user.is_active:
-            return user
-        raise serializers.ValidationError("Invalid credentials")
-
+        model = Task
+        fields = '__all__'  # Include all fields or specify the fields explicitly
