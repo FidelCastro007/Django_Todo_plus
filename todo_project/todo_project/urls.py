b57efe402d8da69e4010a6ccdd0331f041e7063a
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2025-01-04 00:07:54 +0530

    Delete todo_project directory

--- todo_project/todo_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from todo.views import LoginView  # Import the LoginView class-based view
-
-urlpatterns = [
-    path('api/', include('todo.urls')),  # Include API-related routes
-    path('accounts/login/', LoginView.as_view(), name='login'),  # Use LoginView class here
-]
