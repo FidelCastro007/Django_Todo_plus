eab664a15d451163862591a473de5e1daf10c3e2
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2025-01-03 23:23:32 +0530

    Delete todo_project/todo_project directory

--- todo_project/todo_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from todo.views import login_view  # Ensure this path is correct
-
-urlpatterns = [
-    path('api/', include('todo.urls')),  # Include API-related routes
-    # path('accounts/login/', login_view, name='login'),  # Login endpoint
-]
