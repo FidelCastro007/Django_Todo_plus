ca56b1f3bc7cde009be6e4b8c58fcfddc78727f2
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2025-01-04 00:58:00 +0530

    Update Register.js

--- todo-client/src/components/Register.js
+++ todo-client/src/components/Register.js
@@ -14,7 +14,7 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/register/', {
+      const response = await fetch(https://django-todo-plus.onrender.com/api/register/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json'
          },
